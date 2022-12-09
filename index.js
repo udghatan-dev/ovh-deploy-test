2534ab0f61254f6fcb6246b75f830741190b264b
udghatan-dev <85568727+udghatan-dev@users.noreply.github.com>
2022-12-09 12:36:31 +0530

    Deploy Test

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("we are running");
 });
 
-var server = app.listen(process.env.PORT || 9000, () => {
-  console.log("welcome to wa team backend we are listening on " + process.env.PORT || 9000);
+var server = app.listen(process.env.PORT || 9001, () => {
+  console.log("welcome to wa team backend we are listening on " + process.env.PORT || 9001);
 });
